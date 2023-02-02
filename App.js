16e75d116575cceab18337754597cd8bdf7435da
ArnoldITA20 <arnold.grosberg@voco.ee>
2023-02-02 11:35:06 +0200

    Create Splash screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Splash from './src/screens/auth/Splash';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Splash />
+    </SafeAreaView>
+  );
+};
+
+export  default App;
